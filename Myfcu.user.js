d30a81f7d57ba2a44fe23ef9d5542bc09521a15e
fcu-d0441320 <d0441320@fcu.edu.tw>
2019-07-02 11:04:52 +0800

    Update to v1.3
      1. Fix can not show scroll when x-overflow
      2. Fix can not show icon on v1.2
      3. Remove some header which can not use in ajax
      4. Make reszieMenu to be a fuction, not just copy paste.

--- Myfcu.user.js
+++ Myfcu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Myfcu++
-// @namespace    http://nicky.tw/
-// @version      1.2
+// @namespace    https://nicky.tw/
+// @version      1.3
 // @description  Change Myfcu menu to tree mode
 // @author       Dream_Rhythm
 // @match        https://myfcu.fcu.edu.tw/main/webClientMyFcuMain.aspx*
@@ -16,30 +16,18 @@ function tree_maker(parent,name,FCUTree){
 }
 
 function makeFolder(parent,name,folderData){
-    parent.append("<div id="+name+" style='width:270px;'> <img src='https://github.com/fcu-d0441320/UserScript-Myfcu/raw/master/MyTreeFcu_firefox/icons/folder_close.png' width='25' height='25'>"+
+    parent.append("<div id="+name+" style='width:250px;'> <img src='https://github.com/fcu-d0441320/UserScript-Myfcu/raw/master/icons/folder_close.png' width='25' height='25'>"+
             folderData.name+"</div><div class='"+name+"' style='display:none' ></div>");
     $('#'+name).click(function(){
         if($("."+name)[0].style.display=='block'){
             $("."+name).css('display','none');
-            $('#'+name+' img').attr("src","https://github.com/fcu-d0441320/UserScript-Myfcu/raw/master/MyTreeFcu_firefox/icons/folder_close.png");
+            $('#'+name+' img').attr("src","https://github.com/fcu-d0441320/UserScript-Myfcu/raw/master/icons/folder_close.png");
         }
         else{
             $("."+name).css('display','block');
-            $('#'+name+' img').attr("src","https://github.com/fcu-d0441320/UserScript-Myfcu/raw/master/MyTreeFcu_firefox/icons/folder_open.png");
+            $('#'+name+' img').attr("src","https://github.com/fcu-d0441320/UserScript-Myfcu/raw/master/icons/folder_open.png");
         }
-
-        parent[0].style.height='auto';
-        parent[0].style.overflow='';
-        if(parent.height()>document.body.clientHeight*0.75){
-            parent[0].style.height=document.body.clientHeight*0.75+'px';
-            parent[0].style.overflow='scroll';
-            parent[0].style.overflowX='hidden';
-        }
-        else{
-            parent[0].style.height='auto';
-            parent[0].style.overflow='';
-        }
-
+        resizeMenu(parent);
     })
     let array = [];
     $.each(folderData.subs,function(idx,block){
@@ -65,7 +53,7 @@ function makeFolder(parent,name,folderData){
 }
 function makeItem(parent,name,data,isEnd){
     let url = "https://myfcu.fcu.edu.tw/main/webClientMyFcuMain.aspx#/prog/"+data.id;
-    let html="<div style='width:270px;height:23px;'>&nbsp;&nbsp;";
+    let html="<div style='height:23px;'>&nbsp;&nbsp;";
 
     if(isEnd==true)html+="└";
     else html+="├";
@@ -76,7 +64,7 @@ function makeItem(parent,name,data,isEnd){
     else{
         html+="<a href='javascript: return false;' data-url='"+url+"' data-opennew=false";
     }
-    html+=" style='color:black;' >&nbsp;&nbsp;"+data.name+"</a></div>";
+    html+=" style='color:black;white-space: nowrap' >&nbsp;&nbsp;"+data.name+"</a></div>";
     parent.append(html);
 }
 function makeSearchItem(parent,data){
@@ -91,9 +79,9 @@ function makeSearchItem(parent,data){
     if(isfind)return;
 
     let url = "https://myfcu.fcu.edu.tw/main/webClientMyFcuMain.aspx#/prog/"+data.id;
-    let html="<div style='width:270px;height:25px;'>";
+    let html="<div style='height:25px;min-width:250px'>";
 
-    html+="<img src=\"https://github.com/fcu-d0441320/UserScript-Myfcu/raw/master/MyTreeFcu_firefox/icons/myfcu_icon.png\" width=\"18\" height=\"18\">"
+    html+="<img src=\"https://github.com/fcu-d0441320/UserScript-Myfcu/raw/master/icons/myfcu_icon.png\" width=\"18\" height=\"18\">"
 
     if(data.openNew==true){
         html+="<a href='javascript: return false;' data-url='"+data.link+"' data-opennew=true";
@@ -101,7 +89,7 @@ function makeSearchItem(parent,data){
     else{
         html+="<a href='javascript: return false;' data-url='"+url+"' data-opennew=false";
     }
-    html+=" style='color:black;' >"+data.name+"</a></div>";
+    html+=" style='color:black;white-space: nowrap;' >"+data.name+"</a></div>";
     parent.append(html);
 }
 
@@ -111,18 +99,13 @@ function GetAllAPP(){
        url: "https://myfcu.fcu.edu.tw/main/webClientMyFcuMain.aspx/GetAllAppData",
        headers:  {
            "Accept":"application/json, text/plain, */*",
-           "Accept-Encoding":"gzip, deflate, br",
            "Accept-Language":"zh-TW,en-US;q=0.7,en;q=0.3",
            "Cache-Control":"max-age=0, no-cache",
-           "Connection":"keep-alive",
-           "Content-Length":"2",
-           "Host":"myfcu.fcu.edu.tw",
            "Pragma":"no-cache",
            "Content-Type": "application/json; charset=utf-8",
            "User-Agent":"Mozilla/5.0 (Windows NT 10.0;) Gecko/20100101 Firefox/61.0",
-           "Referer":"https://myfcu.fcu.edu.tw/main/webClientMyFcuMain.aspx",
            "Set-Cookie":document.cookie
-             },
+       },
        error: function(error){
            console.log(error)
        },
@@ -141,6 +124,8 @@ function GetAllAPP(){
                })
                $("#menu_search div").eq(idx).hide();
            })
+           resizeMenu($('#menu_oritree'));
+           resizeMenu($('#menu_search'));
        }
     });
 }
@@ -159,8 +144,20 @@ function GetFCUAPPS(){
        },
        success: function(data) {
            tree_maker($('#menu_fcuappstree'),'APPSFolder',data);
+           resizeMenu($('#menu_fcuappstree'));
        }
     });
+}
+
+function resizeMenu(parent){
+    if(parent.height()>document.body.clientHeight*0.75){
+        parent[0].style.height=document.body.clientHeight*0.75+'px';
+        parent[0].style.overflow='scroll';
+    }
+    else{
+        parent[0].style.height='auto';
+        parent[0].style.overflow='';
+    }
 }
 
 if(self.location=='https://myfcu.fcu.edu.tw/main/InfoMyFcuLogin.aspx'){
@@ -204,7 +201,7 @@ else{
     place.children().eq(1).after( '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div style="width: 160px; display: inline-block;"></div>' );
     place.children().eq(2).append(select);
     place.children().eq(2).append('&nbsp;&nbsp;');
-    place.children().eq(2).append('<a id="goSearch" href="javascript:return false;"><img src="https://github.com/fcu-d0441320/UserScript-Myfcu/raw/master/MyTreeFcu_firefox/icons/search.png" width="20" height="20"></a>');
+    place.children().eq(2).append('<a id="goSearch" href="javascript:return false;"><img src="https://github.com/fcu-d0441320/UserScript-Myfcu/raw/master/icons/search.png" width="20" height="20"></a>');
     $('#MenuType').on('change', function() {
         $.each($('#MenuType option'),function(idx,ele){
             $('#'+ele.value).hide();
@@ -224,19 +221,6 @@ else{
                 $("#menu_search div").eq(idx).hide();
             }
         })
-        function resizeMenu(parent){
-            parent[0].style.height='auto';
-            parent[0].style.overflow='';
-            if(parent.height()>document.body.clientHeight*0.75){
-                parent[0].style.height=document.body.clientHeight*0.75+'px';
-                parent[0].style.overflow='scroll';
-                parent[0].style.overflowX='hidden';
-            }
-            else{
-                parent[0].style.height='auto';
-                parent[0].style.overflow='';
-            }
-        }
         resizeMenu($("#menu_search"));
     })
     $("#goSearch").click(function() {
@@ -262,20 +246,9 @@ else{
             else $("html.ng-scope body.ng-scope table.table-root.div-main-show tbody tr td div.ng-binding.div-Balloon").eq(0).addClass('div-Balloon-hide');
         });
     $(window).resize(function() {
-        function resizeMenu(parent){
-            parent[0].style.height='auto';
-            parent[0].style.overflow='';
-            if(parent.height()>document.body.clientHeight*0.75){
-                parent[0].style.height=document.body.clientHeight*0.75+'px';
-                parent[0].style.overflow='scroll';
-                parent[0].style.overflowX='hidden';
-            }
-            else{
-                parent[0].style.height='auto';
-                parent[0].style.overflow='';
-            }
-        }
         resizeMenu($('#menu_oritree'));
+        resizeMenu($('#menu_fcuappstree'));
+        resizeMenu($('#menu_search'));
     });
 
     GetAllAPP();
